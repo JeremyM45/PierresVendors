c9d28041d3c46dbc6652155e8cf8c67a615d6fc7
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-13 09:18:11 -0700

    add more tests for Vendor

--- PierresVendors.Tests/ModelTests/Vendor.Test.cs
+++ PierresVendors.Tests/ModelTests/Vendor.Test.cs
@@ -26,5 +26,21 @@ namespace PierresVendors.Tests
       List<Vendor> allVendors = Vendor.GetVendors();
       Assert.AreEqual(2, allVendors.Count);
     }
+    [TestMethod]
+    public void FindVendor_ShouldReturnASpecificVendorFromName_Vendor()
+    {
+      Vendor thisOne = new Vendor("this one", "des");
+      Vendor notThisOne = new Vendor("not this one", "des");
+      Vendor foundVendor = Vendor.FindVendor("this one");
+      Assert.AreEqual(thisOne, foundVendor);
+    }
+    [TestMethod]
+    public void AddOrder_ShouldAddAnOrderToAVendorsListOfOrders_Vendor()
+    {
+      Vendor newVendor = new Vendor("name", "des");
+      Order newOrder = new Order("test name", "test des");
+      newVendor.AddOrder(newOrder);
+      Assert.AreEqual(1, newVendor.Orders.Count);
+    }
   }
 }
